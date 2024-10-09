ae2c5f48f1270ae104b3bbccc2f38d33a6fb231f
Yohan Alek <yh9dev@gmail.com>
2024-10-09 14:57:20 -0700

    Se creó la sección de home

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,7 +25,11 @@ module.exports = {
 
       backgroundImage: {
         "menu": "url('../imgs/icons/menu.png')",
-        "close": "url('../imgs/icons/close.png')"
+        "close": "url('../imgs/icons/close.png')",
+        "home-400": "linear-gradient(to top, #00000000 90%, #000000 100%), linear-gradient(to bottom, #00000000 0%, #000000 90%), url('../imgs/photos/home-400.jpg')",
+        "home-800": "url('../imgs/photos/home-800.jpg')",
+        "home-1000": "url('../imgs/photos/home-1000.jpg')",
+        "home-1600": "url('../imgs/photos/home-1600.jpg')",
       },
 
     },
