7d41c385f84f59c194cce26b541f311c8de18a61
Yohan Alek <yh9dev@gmail.com>
2024-10-09 12:57:54 -0700

    Se creo la barra de navegación y menu hamburguesa

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,33 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+
+    screens: {
+      "744": "744px",
+      "1000": "1000px",
+      "1600": "1600px",
+    },
+
+    extend: {
+
+      colors: {
+        "yellow-gym": "#FAFF00",
+        "cards-color": "#222222",
+      },
+
+      fontFamily: {
+        "bruno": '"Bruno Ace SC", sans-serif',
+        "mos-bold": '"Made Outer Sans Bold", sans-serif',
+        "mos-light": '"Made Outer Sans Light", sans-serif',
+        "mos-thin": '"Made Outer Sans Thin", sans-serif',
+      },
+
+      backgroundImage: {
+        "menu": "url('../imgs/icons/menu.png')",
+        "close": "url('../imgs/icons/close.png')"
+      },
+
+    },
   },
   plugins: [],
 }
